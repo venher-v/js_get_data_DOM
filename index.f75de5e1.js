072a96b13b40ced40dc305d2b8807047d0e88924
Vitalii Venher <vengervn@gmail.com>
2024-07-16 22:56:21 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const populationDataArr = [
+    ...document.querySelectorAll(".population")
+].map((el)=>+el.innerText.split(",").join(""));
+const total = populationDataArr.reduce((sum, population)=>sum + population, 0);
+const average = total / populationDataArr.length;
+document.querySelector(".total-population").textContent = total.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
